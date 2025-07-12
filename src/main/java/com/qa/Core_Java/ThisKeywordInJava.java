03045bbdcd11c9c545d31a837f727f27b5b2ba49
satyakatharapu96 <104514554+satyakatharapu96@users.noreply.github.com>
2025-07-12 11:59:47 +0530

    inital corejava_Topics

---
+++ src/main/java/com/qa/Core_Java/ThisKeywordInJava.java
@@ -0,0 +1,23 @@
+package com.qa.Core_Java;
+
+public class ThisKeywordInJava {
+
+	String firstname;
+	String lastname;
+	String mailid;
+
+	public ThisKeywordInJava(String firstname, String lastname, String mailid) {
+		this.firstname = firstname;
+		this.lastname = lastname;
+
+	}
+
+	public void displayData() {
+		System.out.println("f_name :" + firstname + " lastname: " + lastname + " mail :" + mailid);
+	}
+
+	public static void main(String[] args) {
+		ThisKeywordInJava th = new ThisKeywordInJava("Ravi", "Boddana", "raviboddana41@gmail.com");
+		th.displayData();
+	}
+}
