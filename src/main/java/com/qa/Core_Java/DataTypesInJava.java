0fd25e30ad827527005c62f8bca8e732497b841b
satyakatharapu96 <104514554+satyakatharapu96@users.noreply.github.com>
2025-07-15 12:01:49 +0530

    Java_Basics

---
+++ src/main/java/com/qa/Core_Java/DataTypesInJava.java
@@ -0,0 +1,32 @@
+package com.qa.Core_Java;
+
+public class DataTypesInJava {
+	public static void main(String[] args) {
+
+		boolean browser = false;
+		
+
+		char c = 'R';
+		short s = 32134;
+		byte b = 127;
+
+		int i = 2053636373;
+		long acc_no = 3106124567898765L;
+		float balance = 789.1234567f;
+		double d = 23.312345678765431222222222222222;
+
+		String name = "Satya"; // datatype and Object
+
+		String browsername = "chrome";
+		System.out.println(browser);
+		System.out.println(s);
+		System.out.println(b);
+		System.out.println(i);
+		System.out.println(acc_no);
+		System.out.println(balance);
+		System.out.println(d);
+		System.out.println(name);
+		System.out.println(browsername);
+	}
+
+}
